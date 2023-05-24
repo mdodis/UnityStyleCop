105f30c103fbe70466a7bcbd12e8e6e94fd11ab5
Michael Dodis <michaeldodisgr@gmail.com>
2023-05-24 10:39:55 +0300

    initial commit

---
+++ Assets/ExampleScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 10;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
